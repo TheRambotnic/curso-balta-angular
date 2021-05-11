d1a7c2681a2e95e603fd06072c71bde9fd0f05d7
TheRambotnic <lucasrafaellonguini09@gmail.com>
2021-05-11 14:55:32 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Tarefa } from 'src/models/Tarefa';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html', // arquivo HTML que utiliza os componentes
+	styleUrls: [
+		'./app.component.css'
+	]
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent implements OnInit {
+	/**
+	 * @params 'lista' | 'adiciona'
+	*/
+	public mode: string = "lista";
+
+	public tarefas: Tarefa[] = [];
+	public titulo: string = "Minhas Tarefas de Hoje";
+	public formTarefa: FormGroup;
+
+	constructor(private fb: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.formTarefa = this.fb.group({
+			tarefaDescritivo: [
+				"", // valor padrão
+
+				// Validators.compose() = array de validações
+				Validators.compose([
+					Validators.minLength(4),
+					Validators.maxLength(60),
+					Validators.required
+				])
+			]
+		});
+
+		this.carregar();
+	}
+
+	// limpa campos do formulário
+	limpar(): void {
+		this.formTarefa.reset();
+	}
+
+	adicionar(): void {
+		const descritivo = this.formTarefa.controls["tarefaDescritivo"].value;
+		const id = this.tarefas.length + 1;
+		this.tarefas.push(new Tarefa(id, descritivo, false));
+		this.salvar();
+		this.limpar();
+	}
+
+	remover(tarefa: Tarefa): void {
+		// procura índice da tarefa passada dentro do array da classe
+		const index = this.tarefas.indexOf(tarefa);
+
+		if (index !== -1) {
+			this.tarefas.splice(index, 1);
+		}
+
+		this.salvar();
+	}
+
+	mudarEstado(mode: string): void {
+		this.mode = mode;
+	}
+
+	marcarFeito(tarefa: Tarefa): void {
+		tarefa.feito = true;
+		this.salvar();
+	}
+
+	marcarNaoFeito(tarefa: Tarefa): void {
+		tarefa.feito = false;
+		this.salvar();
+	}
+
+	salvar(): void {
+		const dado = JSON.stringify(this.tarefas); // .stringify = converte JSON para string
+		localStorage.setItem("tarefa", dado);
+	}
+
+	// localStorage
+	carregar(): void {
+		const dado = localStorage.getItem("tarefa");
+		
+		// ver se não veio nulo do localStorage
+		if (dado) {
+			this.tarefas = JSON.parse(dado); // .parse = converte string para JSON
+		}
+		else {
+			this.tarefas = [];
+		}
+	}
 }
---
+++ src/models/Tarefa.ts
@@ -0,0 +1,3 @@
+export class Tarefa {
+	constructor(public id: number, public descritivo: string, public feito: boolean) {}
+}
